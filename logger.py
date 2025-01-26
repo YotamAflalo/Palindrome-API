bba3e4794db45b5e1afb7a524341c33565f143aa
yotam <yotam.aflalo433@gmail.com>
2025-01-26 15:38:08 +0200

    super refactor and add tests

--- logger.py
+++ logger.py
@@ -1,48 +1,31 @@
 import logging
-from pathlib import Path
 from datetime import datetime
-from pathlib import Path
-from config.config import log_mode
-import sys
 import os
-project_root = str(Path(__file__).parent)
 
-class Logger:
+class Logger(logging.Logger):
     """
-    Singleton logger class that handles both file logging and CloudWatch logging.
+    Logger class that handles file logging.
     Creates a new log file daily in the logs directory.
+
+    For now, it doesn't include much functionality, but it will be possible to add specific functions in the future.
     """
 
-    def __init__(self,log_mode='DEBUG'):
-        # Create logs directory if it doesn't exist
-        log_dir = Path("logs")
-        log_dir.mkdir(exist_ok=True)
+    def __init__(self,logger_name,logs_dir,log_mode='DEBUG'): 
         
         # Create logger
-        self.logger = logging.getLogger("palindrom api")
-        self.logger.setLevel(log_mode)
-        
+        log_level = getattr(logging, log_mode.upper())
+        super().__init__(logger_name, log_level)
+
+        #Insure thet the folder is existing
+        logs_path = os.path.join(logs_dir, "logs")
+        os.makedirs(logs_path, exist_ok=True)
         # Create formatter
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         
         # Create file handler
         current_time = datetime.now().strftime("%Y%m%d")
-        file_handler = logging.FileHandler(os.path.join(project_root, "logs", f"palindrom_api_{current_time}.log"))
+        file_handler = logging.FileHandler(os.path.join(logs_path, f"{logger_name}-{current_time}.log"),mode='a')
         file_handler.setFormatter(formatter)
         
         # Add handler to logger
-        self.logger.addHandler(file_handler)
-    def getLogger(self):
-        return self.logger
-    def debug(self, message: str) -> None:
-         """Log debug level message."""
-         self.logger.debug(message)
-    def info(self, message: str) -> None:
-         """Log info level message."""
-         self.logger.info(message)
-    def eror(self, message: str) -> None:
-         """Log error level message."""
-         self.logger.error(message)
-
-# Create a singleton instance
-logger = Logger(log_mode=log_mode)#.getLogger()
+        self.addHandler(file_handler)
